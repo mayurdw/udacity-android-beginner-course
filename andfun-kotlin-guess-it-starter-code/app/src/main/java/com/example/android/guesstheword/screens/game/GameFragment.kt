fcd3943af0f7530d52443b52c8f42893fd820b22
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2021-06-20 16:40:23 +1200

    Added LiveData & Timer

--- andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -22,6 +22,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
@@ -30,112 +33,59 @@ import com.example.android.guesstheword.databinding.GameFragmentBinding
  * Fragment where the game is played
  */
 class GameFragment : Fragment() {
-
-    // The current word
-    private var word = ""
-
-    // The current score
-    private var score = 0
-
-    // The list of words - the front of the list is the next word to guess
-    private lateinit var wordList: MutableList<String>
-
     private lateinit var binding: GameFragmentBinding
+    private lateinit var gameViewModel: GameViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
         // Inflate view and obtain an instance of the binding class
         binding = DataBindingUtil.inflate(
-                inflater,
-                R.layout.game_fragment,
-                container,
-                false
+            inflater,
+            R.layout.game_fragment,
+            container,
+            false
         )
 
-        resetList()
-        nextWord()
+        gameViewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
-        binding.correctButton.setOnClickListener { onCorrect() }
-        binding.skipButton.setOnClickListener { onSkip() }
-        updateScoreText()
-        updateWordText()
+        gameViewModel.score.observe(this, Observer { newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
+
+        gameViewModel.word.observe(this, Observer { newWord ->
+            binding.wordText.text = newWord
+        })
+
+        gameViewModel.gameFinished.observe(this, Observer { newState ->
+            if( newState ) {
+                gameFinished()
+                gameViewModel.onGameFinishedCompleted()
+            }
+        })
+
+        gameViewModel.time.observe(this, Observer { newTime ->
+            binding.timerText.text = newTime
+        })
+
+        binding.correctButton.setOnClickListener {
+            gameViewModel.onCorrect()
+        }
+        binding.skipButton.setOnClickListener {
+            gameViewModel.onSkip()
+        }
+
         return binding.root
 
-    }
-
-    /**
-     * Resets the list of words and randomizes the order
-     */
-    private fun resetList() {
-        wordList = mutableListOf(
-                "queen",
-                "hospital",
-                "basketball",
-                "cat",
-                "change",
-                "snail",
-                "soup",
-                "calendar",
-                "sad",
-                "desk",
-                "guitar",
-                "home",
-                "railway",
-                "zebra",
-                "jelly",
-                "car",
-                "crow",
-                "trade",
-                "bag",
-                "roll",
-                "bubble"
-        )
-        wordList.shuffle()
     }
 
     /**
      * Called when the game is finished
      */
     private fun gameFinished() {
-        val action = GameFragmentDirections.actionGameToScore(score)
+        val action = GameFragmentDirections.actionGameToScore( gameViewModel.score.value ?: 0 )
         findNavController(this).navigate(action)
-    }
-
-    /**
-     * Moves to the next word in the list
-     */
-    private fun nextWord() {
-        //Select and remove a word from the list
-        if (wordList.isEmpty()) {
-            gameFinished()
-        } else {
-            word = wordList.removeAt(0)
-        }
-        updateWordText()
-        updateScoreText()
-    }
-
-    /** Methods for buttons presses **/
-
-    private fun onSkip() {
-        score--
-        nextWord()
-    }
-
-    private fun onCorrect() {
-        score++
-        nextWord()
-    }
-
-    /** Methods for updating the UI **/
-
-    private fun updateWordText() {
-        binding.wordText.text = word
-
-    }
-
-    private fun updateScoreText() {
-        binding.scoreText.text = score.toString()
     }
 }
---
+++ andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,130 @@
+package com.example.android.guesstheword.screens.game
+
+import android.os.CountDownTimer
+import android.text.format.DateUtils
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+
+    companion object{
+        // These represent different important times
+        // This is the number of milliseconds in a second
+        const val ONE_SECOND = 1000L
+        // This is the total time of the game
+        const val COUNTDOWN_TIME = 60000L
+    }
+
+    private var _time = MutableLiveData<String>()
+    val time : LiveData<String>
+        get() = _time
+
+    // The current word
+    private var _word = MutableLiveData<String>()
+    val word : LiveData<String>
+        get() = _word
+
+    // The current score
+    private var _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
+        get() = _score
+
+    private var _gameFinished = MutableLiveData<Boolean>()
+    val gameFinished : LiveData<Boolean>
+        get() = _gameFinished
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+    private var timer: CountDownTimer
+    private var currentTime = COUNTDOWN_TIME
+
+
+    init {
+        Log.d( "GameViewModel", "GameViewModel created" )
+        this.resetList()
+        this.nextWord()
+        this._score.value = 0
+        this._gameFinished.value = false
+        _time.value = formatTime( currentTime )
+        timer = object : CountDownTimer( COUNTDOWN_TIME, ONE_SECOND ){
+            override fun onTick(millisUntilFinished: Long) {
+                _time.value = formatTime( --currentTime )
+            }
+
+            override fun onFinish() {
+                _gameFinished.value = true
+            }
+        }.start()
+    }
+
+    private fun formatTime( time : Long ): String? {
+        return DateUtils.formatElapsedTime( time )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer.cancel()
+        Log.d( "GameViewModel", "GameViewModel cleared" )
+    }
+
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            resetList()
+        }
+        _word.value = wordList.removeAt(0)
+    }
+
+    /** Methods for buttons presses **/
+
+    fun onSkip() {
+        this._score.value = (score.value)?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        this._score.value = (score.value)?.plus(1)
+        nextWord()
+    }
+
+    fun onGameFinishedCompleted() {
+        this._gameFinished.value = false
+    }
+
+}
