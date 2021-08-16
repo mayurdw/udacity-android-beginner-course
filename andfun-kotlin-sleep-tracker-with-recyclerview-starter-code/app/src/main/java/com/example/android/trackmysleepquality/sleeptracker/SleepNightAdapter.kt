bfd47d5ff8d687f6e9ac54f804e5270d387e5e66
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2021-08-16 21:30:25 +1200

    Done for the day

--- andfun-kotlin-sleep-tracker-with-recyclerview-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ andfun-kotlin-sleep-tracker-with-recyclerview-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -45,22 +45,8 @@ class SleepNightAdapter : ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(
         fun bind(
             item: SleepNight
         ) {
-            val res = itemView.context.resources
-            listItemSleepNightBinding.sleepLength.text =
-                convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-            listItemSleepNightBinding.qualityString.text = convertNumericQualityToString(item.sleepQuality, res)
-
-            listItemSleepNightBinding.qualityImage.setImageResource(
-                when (item.sleepQuality) {
-                    0 -> R.drawable.ic_sleep_0
-                    1 -> R.drawable.ic_sleep_1
-                    2 -> R.drawable.ic_sleep_2
-                    3 -> R.drawable.ic_sleep_3
-                    4 -> R.drawable.ic_sleep_4
-                    5 -> R.drawable.ic_sleep_5
-                    else -> R.drawable.ic_sleep_active
-                }
-            )
+            listItemSleepNightBinding.sleepNight = item
+            listItemSleepNightBinding.executePendingBindings()
         }
 
         companion object {
